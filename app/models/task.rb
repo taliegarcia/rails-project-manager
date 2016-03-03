0d6892dee98a7453c18d89d77065f9641d9b887a
taliegarcia <taliegarcia@gmail.com>
2016-03-03 11:07:34 -0800

    tasks; made name and description required on tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ActiveRecord::Base
   validates :status, :inclusion=> { :in => %w(unstarted started finished), message: "%{value} is not a valid status" }
+  validates :name, :description, presence: true
 
   belongs_to :project
   validates :project, presence: true
-
 
 
 end
